3a118c98d3c721663a03fdc1af20e095defedca5
Ashley Sands <ashsands1@amstrike.com>
2020-06-14 03:16:42 +0100

    fixed typo in file name

--- CI-root/main-ci-root.py
+++ CI-root/main-ci-root.py
@@ -22,13 +22,13 @@ if __name__ == "__main__":
 
     cmd = "cd /root/project/unityBuild;" \
           "source ./preBuild.sh;" \
-          "./before_build.sh;" \
+          "./before_script.sh;" \
           "./build.default2.sh;"
 
     print(cmd)
     print("Start Build Process, Hold Tight...")
 
-    #for line in run_process( cmd ):
-    #    print( line )
+    for line in run_process( cmd ):
+        print( line )
 
     print( "Build Complete ")
