de471b53f0b3898b82694954039a5e43a7602631
Ashley Sands <ashsands1@amstrike.com>
2020-06-14 05:17:44 +0100

    moved build config into ci root. i need to change it a little

--- CI-Host/main-ci-host.py
+++ CI-Host/main-ci-host.py
@@ -11,33 +11,6 @@ def read_file( file_name ):
 def get_dict_from_json( file_name ):
     return json.loads( read_file( file_name ) )
 
-def build_config_sh(config):
-    """ builds the sh file for each stage.
-    :param config:  the config dict
-    """
-
-    env = ["# set up the environment"]
-    # build the environment section of the sh file
-    for e in config["environment"]:
-        line = "export {0}={1}".format(e, config["environment"][e])
-        env.append(line)
-
-    stages = []
-    stage_names = []
-
-    # build each stage
-    for p in config["pipeline"]:
-        stage = [ "# stage "+p["name"] ]
-        for c in p["commands"]:
-            stage.append( c )
-        stages.append(stage)
-
-    # it worth noting that if we are going to bind mount directories into the image the configs
-    # need to be unique folder, to insure that files are not over writen, before or during use.
-    with open("stage.txt", mode="w") as file:
-        file.write( '\n'.join( stages[0] ) )
-
-
 if __name__ == "__main__":
 
     print("Loading Config...")
--- CI-root/main-ci-root.py
+++ CI-root/main-ci-root.py
@@ -5,6 +5,32 @@
 import subprocess
 import common
 
+def build_config_sh(config):
+    """ builds the sh file for each stage.
+    :param config:  the config dict
+    """
+
+    env = ["# set up the environment"]
+    # build the environment section of the sh file
+    for e in config["environment"]:
+        line = "export {0}={1}".format(e, config["environment"][e])
+        env.append(line)
+
+    stages = []
+    stage_names = []
+
+    # build each stage
+    for p in config["pipeline"]:
+        stage = [ "# stage "+p["name"] ]
+        for c in p["commands"]:
+            stage.append( c )
+        stages.append(stage)
+
+    # it worth noting that if we are going to bind mount directories into the image the configs
+    # need to be unique folder, to insure that files are not over writen, before or during use.
+    with open("stage.txt", mode="w") as file:
+        file.write( '\n'.join( stages[0] ) )
+
 if __name__ == "__main__":
 
     cmd = "cd /root/project/unityBuild;" \
@@ -15,7 +41,7 @@ if __name__ == "__main__":
     print(cmd)
     print("Start Build Process, Hold Tight...")
 
-    for line in common.run_process( cmd, shell="sh" ):
+    for line in common.run_process( cmd, shell="bash" ):
         print( line )
 
     print( "Build Complete " )
