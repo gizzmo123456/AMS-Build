61908132adcf50b60703a29ba88f4bde56b6cd9c
Ashley Sands <ashsands1@amstrike.com>
2020-06-14 05:18:18 +0100

    gave all magic values vars :)

--- CI-Host/main-ci-host.py
+++ CI-Host/main-ci-host.py
@@ -13,8 +13,23 @@ def get_dict_from_json( file_name ):
 
 if __name__ == "__main__":
 
-    print("Loading Config...")
+    print( "Loading Config..." )
+    default_shell = "sh"
     config = get_dict_from_json( "../CI-config/test.json" )
+
+    local_cof = {
+        "ci-root": "${HOME}/unity-ci/CI-root/",
+        "ci-config": "${HOME}/unity-ci/CI-config/test.json",
+        "project": "${HOME}/project/"
+    }
+
+    docker_cof = {
+        "ci-root-dest": "/root/CI-root:ro",                     # ci-tool mouth point as read only
+        "ci-config-dest": "/root/CI-config/pipeline.json:ro",   # config mouth point as read only
+        "project-dest": "/root/project:ro",                     # project mount point as read only
+        "image": config["docker"]["iamge"],
+        "args": config["docker"]["args"]
+    }
 
     print("Docker:", config["docker"])
     print("Environment:", config["environment"])
@@ -26,7 +41,7 @@ if __name__ == "__main__":
 
     error = False
 
-    for line in common.run_process( "sudo docker image inspect {image}".format(image="gableroux/unity3d:2018.4.2f1-windows"), shell="sh" ):     # if the first line is an empty list (ie. [] ) no image exist
+    for line in common.run_process( "sudo docker image inspect {image}".format(image=docker_cof["image"]), shell=default_shell ):     # if the first line is an empty list (ie. [] ) no image exist
         if line == "[]":
             error = True
             continue
@@ -39,7 +54,7 @@ if __name__ == "__main__":
 
     if error:   # attempt to pull the image
         print("Attempting pull image from docker...")
-        for line in common.run_process( "sudo docker pull {image}".format(image="gableroux/unity3d:2018.4.2f1-windows"), shell="sh" ):          # if the fist word of the first line is error this image does not exist in the repo.
+        for line in common.run_process( "sudo docker pull {image}".format(image=docker_cof["image"]), shell=default_shell ):          # if the fist word of the first line is error this image does not exist in the repo.
             if error and line.split( " " )[ 0 ].lower() != "error":
                 error = False
                 print( "Found!, Pulling image." )
@@ -55,15 +70,17 @@ if __name__ == "__main__":
     # and any files created are not passed back into the host system.
     dockerRun = "sudo docker run " \
                 "{args} " \
-                "-v ${HOME}/unity-ci/CI-root:/root/CI-root:ro " \
-                "-v ${HOME}/unity-ci/CI-config/test.json:/root/CI-config/pipeline.json " \
+                "-v {project_path}:{project_dest}" \
+                "-v {ci_root_path}:{ci_root_dest} " \
+                "-v {ci_config_path}:{ci_config_dest} " \
                 "{image} " \
-                "python3 /root/CI-root/main-ci-root.py".format(args="-it --rm -v ${HOME}/project:/root/project",
-                                                               image="gableroux/unity3d:2018.4.2f1-windows",
-                                                               HOME="{HOME}" )
+                "{cmd}".format(args=docker_cof["args"],
+                               project_path=local_cof["project"],       project_dest=docker_cof["project-dest"],
+                               ci_root_path=local_cof["ci-root"],       ci_root_dest=docker_cof["ci-root-dest"],
+                               ci_config_path=local_cof["ci-config"],   ci_config_dest=docker_cof["ci-config-dest"],
+                               image="gableroux/unity3d:2018.4.2f1-windows",
+                               cmd="python3 /root/CI-root/main-ci-root.py")
     print(dockerRun)
-    for line in common.run_process( dockerRun, shell="sh" ):  # if the fist word of the first line is error this image does not exist in the repo.
+    for line in common.run_process( dockerRun, shell=default_shell ):  # if the fist word of the first line is error this image does not exist in the repo.
         print(line)
 
-    for line in common.run_process( "echo 'Im the Precess :)' ", shell="sh" ):
-        print( line )
