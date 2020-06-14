ed08dd5412fd776a6626c7eed4795b6a59a91adb
Ashley Sands <ashsands1@amstrike.com>
2020-06-14 03:06:39 +0100

    Added the basic cmd for the contatiner build :)

--- CI-root/main-ci-root.py
+++ CI-root/main-ci-root.py
@@ -1,6 +1,33 @@
 # The CI-root directory needs to mounted to /root/CI-root within the container
 #
 
+import subprocess
+
+def run_process( command ):  # Todo: make this common :)
+
+    process = subprocess.Popen( ['python3', '-c', 'import os;os.system("{0}")'.format(command)], stdout=subprocess.PIPE )
+
+    while True:
+        line = process.stdout.readline().decode( "utf-8" )
+        if not line:
+            break
+        else:
+            if line[-1:] == '\n':   # remove the new line char so we don't get double new lines :)
+                line = line[:-1]
+            yield line
+
+    process.kill()
 
 if __name__ == "__main__":
+
+    cmd = "cd /root/project/unityBuild\n" \
+          "source ./preBuild.sh\n" \
+          "./before_build.sh\n" \
+          "./build.default2.sh\n"
+
     print("Start Build Process, Hold Tight...")
+
+    for line in run_process( cmd ):
+        print( line )
+
+    print( "Build Complete ")
