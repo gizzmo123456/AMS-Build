5e1ce04522d6eb0aa4743c636a80f2e1624b6155
Ashley Sands <ashsands1@amstrike.com>
2020-06-14 06:30:54 +0100

    add a couple more vars and cleaned up a little

--- CI-Host/main-ci-host.py
+++ CI-Host/main-ci-host.py
@@ -2,6 +2,7 @@
 
 import json
 import common
+import threading
 
 def read_file( file_name ):
 
@@ -14,31 +15,40 @@ def get_dict_from_json( file_name ):
 if __name__ == "__main__":
 
     print( "Loading Config..." )
+
+    project_name = "example_project"
     default_shell = "sh"
+
+
     config = get_dict_from_json( "../CI-projects/exampleProject/pipeline.json" )
 
     local_cof = {
-        "ci-root": "${HOME}/unity-ci/CI-root/",
-        "ci-config": "${HOME}/unity-ci/CI-projects/exampleProject/pipeline.json",
-        "project": "${HOME}/project/"
+        "ci-root"       : "${HOME}/unity-ci/CI-root/",
+        # TODO: master needs to be the name of the copied directory
+        "ci-config"     : "${HOME}/unity-ci/CI-projects/exampleProject/master/pipeline.json",
+        "project"       : "${HOME}/unity-ci/CI-projects/exampleProject/master/project_source/",
+        "build_output"  : "${HOME}/unity-ci/CI-projects/exampleProject/master/build",
+        "output"        : "${HOME}/unity-ci/CI-projects/exampleProject/master/output.txt",
     }
 
     docker_cof = {
-        "ci-root-dest": "/root/CI-root:ro",                     # ci-tool mouth point as read only
-        "ci-config-dest": "/root/CI-config/pipeline.json:ro",   # config mouth point as read only
-        "project-dest": "/root/project",                        # project mount point
+        "ci-root-dest"  : "/root/CI-root:ro",                     # ci-tool mouth point as read only
+        "ci-config-dest": "/root/CI-config/pipeline.json:ro",     # config mouth point as read only
+        "project-dest"  : "/root/project",                        # project mount point
+        "build_output"  : "",
+        "output"        : "",
         "image": config["docker"]["image"],
         "args": config["docker"]["args"]
     }
 
-    print("Docker:", config["docker"])
-    print("Environment:", config["environment"])
-    print("Pipeline", config["pipeline"])
+    print("Local Config:", local_cof)
+    print("Docker Config:", docker_cof)
 
     print("="*24)
 
     print("Verifying image exist...")
 
+    # find if we have the docker image locally if not attempt to pull the image from docker
     error = False
 
     for line in common.run_process( "sudo docker image inspect {image}".format(image=docker_cof["image"]), shell=default_shell ):     # if the first line is an empty list (ie. [] ) no image exist
@@ -65,6 +75,7 @@ if __name__ == "__main__":
         print("Unable to pull docker image. Build Failed, exiting...")
         exit(1)
 
+    print("="*24)
     print("Deploying docker container, please wait...")
     # We must mark the CI-root as read only (:ro) to insure that files are not modified
     # and any files created are not passed back into the host system.
@@ -80,7 +91,9 @@ if __name__ == "__main__":
                                ci_config_path=local_cof["ci-config"],   ci_config_dest=docker_cof["ci-config-dest"],
                                image="gableroux/unity3d:2018.4.2f1-windows",
                                cmd="python3 /root/CI-root/main-ci-root.py")
-    print(dockerRun)
+
+    print("Running docker cmd:", dockerRun)
+
     for line in common.run_process( dockerRun, shell=default_shell ):  # if the fist word of the first line is error this image does not exist in the repo.
         print(line)
 
