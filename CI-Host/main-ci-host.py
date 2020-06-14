30fb2590a9bb213796268e802213b280c5b6f41a
Ashley Sands <ashsands1@amstrike.com>
2020-06-14 23:35:03 +0100

    tided up directorys and mounted volumes

--- CI-Host/main-ci-host.py
+++ CI-Host/main-ci-host.py
@@ -16,29 +16,38 @@ if __name__ == "__main__":
 
     print( "Loading Config..." )
 
-    project_name = "example_project"
+    project_name = "exampleProject"
     default_shell = "sh"
 
+    base_directory = "${HOME}/unity-ci"
+    project_directory = base_directory + "/CI-project"
+    build_project_directory = "{project_dir}/{project}/{build}".format(project_dir=project_directory,
+                                                                       project="exampleProject",
+                                                                       build="master")
 
-    config = get_dict_from_json( "../CI-projects/exampleProject/master/pipeline.json" )
+    relevant_project_directory = "../CI-projects"   # relevant to the CI-host file :)
+
+    # always use the master config file when queuing builds.
+    config = "{relv_proj_dir}/{project}/master/pipeline.json".format(relv_proj_dir=relevant_project_directory, project=project_name)
+    config = get_dict_from_json( config )
 
     local_cof = {
-        "ci-root"       : "${HOME}/unity-ci/CI-root/",
+        "ci-root"       : "{root_dir}/CI-root/".format(root_dir=base_directory),
         # TODO: master needs to be the name of the copied directory
-        "ci-config"     : "${HOME}/unity-ci/CI-projects/exampleProject/master/pipeline.json",
-        "project"       : "${HOME}/unity-ci/CI-projects/exampleProject/master/project_source/",
-        "build_output"  : "${HOME}/unity-ci/CI-projects/exampleProject/master/build",
-        "output"        : "${HOME}/unity-ci/CI-projects/exampleProject/master/output.txt",
+        "ci-config"     : build_project_directory+"/pipeline.json",
+        "project"       : build_project_directory+"/project_source/",
+        "build-output"  : build_project_directory+"/build",
+        "stdout"        : build_project_directory+"/output.txt"
     }
 
     docker_cof = {
-        "ci-root-dest"  : "/root/CI-root:ro",                     # ci-tool mouth point as read only
-        "ci-config-dest": "/root/CI-config/pipeline.json:ro",     # config mouth point as read only
-        "project-dest"  : "/root/project",                        # project mount point
-        "build_output"  : "",
-        "output"        : "",
-        "image"         : config["docker"]["image"],
-        "args"          : config["docker"]["args"]
+        "ci-root-dest"      : "/root/CI-root:ro",                                       # ci-tool mouth point as read only
+        "ci-config-dest"    : "/root/CI-root/pipeline.json:ro",                         # config mouth point as read only
+        "project-dest"      : "/root/project",                                          # project source mount point
+        "build-output-dest" : "/root/project/unityBuild/Builds/StandaloneWindows",      # build output mount point
+        "stdout-dest"       : "/root/CI-root/output/output.txt",                        # stdout mount point
+        "image"             : config["docker"]["image"],
+        "args"              : config["docker"]["args"]
     }
 
     print("Local Config:", local_cof)
@@ -84,11 +93,15 @@ if __name__ == "__main__":
                 "-v {project_path}:{project_dest} " \
                 "-v {ci_root_path}:{ci_root_dest} " \
                 "-v {ci_config_path}:{ci_config_dest} " \
+                "-v {ci_build_path}:{ci_build_dest}" \
+                "-v {ci_stdout_path}:{ci_stdout_dest}" \
                 "{image} " \
                 "{cmd}".format(args=docker_cof["args"],
                                project_path=local_cof["project"],       project_dest=docker_cof["project-dest"],
                                ci_root_path=local_cof["ci-root"],       ci_root_dest=docker_cof["ci-root-dest"],
                                ci_config_path=local_cof["ci-config"],   ci_config_dest=docker_cof["ci-config-dest"],
+                               ci_build_path=local_cof["build-output"], ci_build_dest=docker_cof["build-output-dest"],
+                               ci_stdout_path=local_cof["stdout"],      ci_stdout_dest=docker_cof["stdout-dest"],
                                image="gableroux/unity3d:2018.4.2f1-windows",
                                cmd="python3 /root/CI-root/main-ci-root.py")
 
---
+++ TODO.md
@@ -0,0 +1,14 @@
+# TODO.
+
+- [ ] Finishing mapping file path
+- [ ] Add copy master, to build folder
+- [ ] Add build log
+- [ ] redesign the json file.
+
+- [ ] Add Basic webhook
+- [ ] add simple webpage
+
+============================================
+
+- [ ] Add Job Queue
+- [ ]  
