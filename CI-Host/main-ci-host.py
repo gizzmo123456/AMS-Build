0b7a0e115447d1e8bde150a6aab832dd7f94ebce
Ashley Sands <ashsands1@amstrike.com>
2020-06-15 03:16:06 +0100

    added debug to the main host py

--- CI-Host/main-ci-host.py
+++ CI-Host/main-ci-host.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import DEBUG
 import build_task
 import common
 import threading
 
 if __name__ == "__main__":
 
-   task = build_task.BuildTask("exampleProject", "master")
-   task.execute()
+    DEBUG = DEBUG.LOGS().init()
+
+    task = build_task.BuildTask("exampleProject", "master")
+    task.execute()
+
+    DEBUG.LOGS().close()
