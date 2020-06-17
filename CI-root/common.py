b373f98f4402a1754bfcc373179b40081ab78f24
Ashley Sands <ashsands1@amstrike.com>
2020-06-17 12:19:53 +0100

    updated Command file in root.
    i will fix this shortly

--- CI-root/common.py
+++ CI-root/common.py
@@ -2,18 +2,33 @@
 # Common function between CI-host and root.
 
 import subprocess
+import json
 
 def run_process( command, shell="python3" ):
 
-    process = subprocess.Popen( [shell, '-c', command], stdout=subprocess.PIPE )
+    process = subprocess.Popen( [shell, '-c', command], stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
 
     while True:
-        line = process.stdout.readline().decode( "utf-8" )
-        if not line:
+
+        line_out = process.stdout.readline().decode( "utf-8" )
+
+        if not line_out:
             break
-        else:
-            if line[-1:] == '\n':   # remove the new line char so we don't get double new lines :)
-                line = line[:-1]
-            yield line
+        elif line_out:
+            # remove the new line char :)
+            if line_out[-1] == "\n":
+                line_out = line_out[:-1]
+
+            yield line_out
 
     process.kill()
+
+def read_file( file_name ):
+
+    with open(file_name) as file:
+        return ''.join( file.readlines() )
+
+
+def get_dict_from_json( file_name ):
+
+    return json.loads( read_file( file_name ) )
