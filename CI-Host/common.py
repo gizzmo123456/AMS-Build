3c8d5cd95e3a8a9fe108e266f183c4ea43e0f23f
Ashley Sands <ashsands1@amstrike.com>
2020-06-19 17:44:47 +0100

    Added write file and added lock file to both read and write

--- CI-Host/common.py
+++ CI-Host/common.py
@@ -3,6 +3,9 @@
 
 import subprocess
 import json
+from filelock import FileLock
+import DEBUG
+_print = DEBUG.LOGS.print
 
 def run_process( command, shell="python3" ):
 
@@ -26,12 +29,47 @@ def run_process( command, shell="python3" ):
 
     process.kill()
 
-def read_file( file_name ):
+def read_file( file_name, lock=False ):
+
+    lock_file = FileLock( file_name+".lock" )
+
+    if lock:
+        lock_file.acquire()
 
     with open(file_name) as file:
-        return ''.join( file.readlines() )
+        data = ''.join( file.readlines() )
 
+    if lock:
+        lock_file.release()
+
+    return data
 
 def get_dict_from_json( file_name ):
 
     return json.loads( read_file( file_name ) )
+
+def write_file( filepath, string, append=False, lock=True ):
+
+    lock_file = FileLock( filepath+".lock" )
+    mode = 'w'
+    if append:
+        mode = 'a'
+
+    if lock:
+        lock_file.acquire()
+
+    with open( filepath, mode ) as file:
+        file.write( string )
+
+    if lock:
+        lock_file.release()
+
+def create_json_file(filepath, data):
+
+    try:
+        fwrite = json.dumps( data )
+    except Exception as e:
+        _print( "Bad Data", e, DEBUG.LOGS.MSG_TYPE_ERROR )
+        return
+
+    write_file( filepath, fwrite)
