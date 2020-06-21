30d54907163a636734286ab9b8060c4c37d374d6
Ashley Sands <ashsands1@amstrike.com>
2020-06-21 14:34:48 +0100

    added debug, for some reason html is being printed when theres no content :|

--- CI-Host/www_page.py
+++ CI-Host/www_page.py
@@ -111,6 +111,7 @@ class WWWPage:
         if self.file_name is None:          # return the raw json data
             page_output = json.dumps( content )
         elif isinstance( content, list ):   # if content is list, we need to return the template for all elements
+            print("------CONTENT LEN ", len(content), " CONTENT: ", content)
             page_output = ""
             for c in content:
                 page_output += www_page.load_template().format( **www_page.build_content( c ) )
