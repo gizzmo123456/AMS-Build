8bf2a8bac7bd96005ba95d9cb22f0f21ca933c7b
Ashley Sands <ashsands1@amstrike.com>
2020-06-23 22:35:51 +0100

    add js debug line

--- CI-Host/www/js/pageLoader.js
+++ CI-Host/www/js/pageLoader.js
@@ -1,23 +1,25 @@
 
 var refreshRate = 20;   //seconds
 
-var loadContent = function(url, responceElem){
+var loadContent = function(url, responceElemId){
 
     var request = new XMLHttpRequest();
-            request.onreadystatechange = function()
+        request.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
             {
-                if (this.readyState == 4 && this.status == 200)
-                {
-                    responceElem.innerHTML = this.responseText;
-                }
-                else if ( this.status >= 300)
-                {
-                    responceElem.innerHTML = ` Error: ${this.status}`
-                }
-            };
+                responceElemId.innerHTML = this.responseText;
+            }
+            else if ( this.status >= 300)
+            {
+                responceElemId.innerHTML = ` Error: ${this.status}`
+            }
+            console.log( `URL ${url} ||| Ready State ${this.readyState} ||| Status ${this.status}` )
+        };
 
-            request.open("GET", url, true);
-            request.send();
+        request.open("GET", url, true);
+        request.send();
+        console.log(` url ${url} sent `)
 
 }
 
