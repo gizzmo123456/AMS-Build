22d18ca42677f4054e323c3727e7b5105f98596e
Ashley Sands <ashsands1@amstrike.com>
2020-08-01 22:30:15 +0100

    added sudo's

--- amsci.sh
+++ amsci.sh
@@ -2,6 +2,6 @@
 
 # make sure the dependencies are install
 # and run the application
-pip3 install -r ./requirements.txt
+sudo pip3 install -r ./requirements.txt
 cd ./CI-Host/
-python3 ./main-ci-host.py
+sudo python3 ./main-ci-host.py
