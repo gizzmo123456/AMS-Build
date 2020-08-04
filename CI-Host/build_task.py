8d02154666a6d1d913c6bfc1b209a296beb70a8c
Ashley Sands <ashsands1@amstrike.com>
2020-08-04 19:07:18 +0100

    another forgoten space smh

--- CI-Host/build_task.py
+++ CI-Host/build_task.py
@@ -195,13 +195,13 @@ class BuildTask:
         """deploys the docker container to run the build"""
 
         dockerRun = "sudo docker run " \
-                    "{args} --name {container_name} " \
+                    "--name {container_name} {args} " \
                     "-v {project_path}:{project_dest} " \
                     "-v {ci_root_path}:{ci_root_dest} " \
                     "-v {ci_config_path}:{ci_config_dest} " \
                     "-v {ci_build_path}:{ci_build_dest} " \
-                    "{image}" \
-                    "{cmd}".format( args=self.docker_cof[ "args" ], container_name=self.format_values["container_name"].lower(),
+                    "{image} " \
+                    "{cmd}".format( container_name=self.format_values["container_name"].lower(), args=self.docker_cof[ "args" ],
                                     project_path=self.local_cof[ "project" ], project_dest=self.docker_cof[ "project-dest" ],
                                     ci_root_path=self.local_cof[ "ci-root" ], ci_root_dest=self.docker_cof[ "ci-root-dest" ],
                                     ci_config_path=self.local_cof[ "ci-config" ], ci_config_dest=self.docker_cof[ "ci-config-dest" ],
