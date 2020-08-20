f06bbb1136998857c7ffd1c3c7bbc5b05bb8d47a
Ashley Sands <ashsands1@amstrike.com>
2020-08-21 00:15:34 +0100

    Started the changing the AMSCI.sh to the install file

--- ams-build-setup.sh
+++ ams-build-setup.sh
@@ -2,6 +2,6 @@
 
 # make sure the dependencies are install
 # and run the application
-pip3 install -r ./requirements.txt
-cd ./CI-Host/
-python3 ./main-ci-host.py
+# pip3 install -r ./requirements.txt
+ROOT_DIRECTORY=$PWD
+echo ROOT_DIRECTORY
