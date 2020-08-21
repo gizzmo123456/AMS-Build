67ebb3c1caf75732acff551ad585a3a4c5d914f1
Ashley Sands <ashsands1@amstrike.com>
2020-08-21 01:02:43 +0100

    Added Create Root Directory config to .sh

--- ams-build-setup.sh
+++ ams-build-setup.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
 
-# make sure the dependencies are install
-# and run the application
-# pip3 install -r ./requirements.txt
+# install Python dependices
+# create the ams_path_config file
+# add ams-build  alias
+
 ROOT_DIRECTORY=$PWD
-echo ROOT_DIRECTORY
+
+echo "Installing Python 3 Requirements..."
+pip3 install -r ./requirements.txt
+
+echo "Setting AMS-Build Root Directory..."
+echo '{ "default_shell": "sh", "base_directory": "'$ROOT_DIRECTORY'"}' > $ROOT_DIRECTORY'/CI-Host/data/test_path_conf.json'
+
+echo "Adding Alias..."
+
+
+echo "Setup Complete"
+echo "Use Command 'amsbuild' to launch AMS-Build Application, (sudo rights required)"
