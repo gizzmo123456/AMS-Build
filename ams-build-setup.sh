740a6e1e3ada4e3a298cd692efd3b8d3b0fa4f35
Ashley Sands <ashsands1@amstrike.com>
2020-08-21 01:14:13 +0100

    Every think seams fine switching to the atuch bash file

--- ams-build-setup.sh
+++ ams-build-setup.sh
@@ -13,9 +13,9 @@ echo "Setting AMS-Build Root Directory..."
 echo '{ "default_shell": "sh", "base_directory": "'$ROOT_DIRECTORY'"}' > $ROOT_DIRECTORY'/CI-Host/data/ams_path_conf.json'
 
 echo "Adding Alias..."
-echo "\n\n" >> ~/world.txt
-echo "# Alias to launch AMS-Build" >> ~/world.txt
-echo 'alias amsbuild="cd '$ROOT_DIRECTORY'/CI-Host; sudo python3 main-ci-host.py;"' >> ~/world.txt
+echo "\n\n" >> ~/.bashrc
+echo "# Alias to launch AMS-Build" >> ~/.bashrc
+echo 'alias amsbuild="cd '$ROOT_DIRECTORY'/CI-Host; sudo python3 main-ci-host.py;"' >> ~/.bashrc
 
 echo "Setup Complete"
 echo "Use Command 'amsbuild' to launch AMS-Build Application, (sudo rights required)"
