16a72df650c7cf06b6ad95743d5d4e8dd9d08a92
Ashley Sands <ashsands1@amstrike.com>
2020-08-21 01:09:41 +0100

    added alias to setup.sh

--- ams-build-setup.sh
+++ ams-build-setup.sh
@@ -10,10 +10,12 @@ echo "Installing Python 3 Requirements..."
 pip3 install -r ./requirements.txt
 
 echo "Setting AMS-Build Root Directory..."
-echo '{ "default_shell": "sh", "base_directory": "'$ROOT_DIRECTORY'"}' > $ROOT_DIRECTORY'/CI-Host/data/test_path_conf.json'
+echo '{ "default_shell": "sh", "base_directory": "'$ROOT_DIRECTORY'"}' > $ROOT_DIRECTORY'/CI-Host/data/ams_path_conf.json'
 
 echo "Adding Alias..."
-
+echo "\n\n" >> ~/world.txt
+echo "# Alias to launch AMS-Build" >> ~/world.txt
+echo 'alias amsbuild="cd '$ROOT_DIRECTORY'/CI-Host; sudo python3 main-ci-host.py;"' >> ~/world.txt
 
 echo "Setup Complete"
 echo "Use Command 'amsbuild' to launch AMS-Build Application, (sudo rights required)"
