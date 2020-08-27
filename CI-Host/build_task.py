538d6e119a394dbe928e5e2c3d9f2241de4c5d7a
Ashley Sands <ashsands1@amstrike.com>
2020-08-27 21:36:25 +0100

    moved DOCKER_ROOT_DIRECTORY const to the pipeline file

--- CI-Host/build_task.py
+++ CI-Host/build_task.py
@@ -197,10 +197,10 @@ class BuildTask:
 
         self.docker_cof = {
             "container_name": project_name.lower() + self.format_values["build_hash"].lower(),
-            "stop_timeout": self.get_config_value( "docker", "stop-timeout", default_value=10 ),    # the amount of time to wait until SIGKILL is sent after SIGTERM is sent (can be None)
-            "ci-root-dest": DOCKER_ROOT_DIRECTORY + "/CI-root:ro",                                  # ci-tool mouth point as read only
-            "project-dest": self.config[ "docker" ][ "project-dest" ],                              # project source mount point
-            "build-output-dest": self.config[ "docker" ][ "build-output-dest" ],                    # build output mount point
+            "stop_timeout": self.get_config_value( "docker", "stop-timeout", default_value=10 ),                      # the amount of time to wait until SIGKILL is sent after SIGTERM is sent (can be None)
+            "ci-root-dest": self.get_config_value( "docker", "ams-container-dest", default_value="/root/AMS-Build"),  # ci-tool mouth point as read only
+            "project-dest": self.config[ "docker" ][ "project-dest" ],                                                # project source mount point
+            "build-output-dest": self.config[ "docker" ][ "build-output-dest" ],                                      # build output mount point
             "image": self.config[ "docker" ][ "image" ],
             "args": self.config[ "docker" ][ "args" ]
         }
@@ -301,7 +301,7 @@ class BuildTask:
                                     ci_root_path=self.local_cof[ "ci-root" ], ci_root_dest=self.docker_cof[ "ci-root-dest" ],
                                     ci_build_path=self.local_cof[ "build-output" ], ci_build_dest=self.docker_cof[ "build-output-dest" ],
                                     image=self.docker_cof["image"],
-                                    cmd="python3 {docker_ci_root}/CI-root/main-ci-root.py".format( docker_ci_root=DOCKER_ROOT_DIRECTORY ) )
+                                    cmd="python3 {docker_ci_root}/CI-root/main-ci-root.py".format( docker_ci_root=self.docker_cof[ "ci-root-dest" ] ) )
 
         _print( "DOCKER RUN:\n", dockerRun, output_filename=self.stdout_filepath, console=False )
 
--- CI-Host/const.py
+++ CI-Host/const.py
@@ -8,7 +8,6 @@ APP_VERSION = "1.0.1"
 DEFAULT_SHELL = config_manager.ConfigManager.get( "default_shell", "sh" )
 BASE_DIRECTORY = config_manager.ConfigManager.get( "base_directory", "${HOME}/ams-build")
 
-DOCKER_ROOT_DIRECTORY = "/root/AMS-CI"              # Should be in the pipeline?? # This is the directory that the CI-config and CI-Root are mounted
 PROJECT_DIRECTORY = BASE_DIRECTORY + "/CI-projects"
 RELEVENT_PROJECT_PATH = "../CI-projects"            # relevant to the CI-Host folder
 
