44e4bd295d571fbfae89cac4fcc2f5ee23dfb2b4
Ashley Sands <ashsands1@amstrike.com>
2021-06-15 16:24:31 +0100

    moved create jobs from piprline into the job queue!

--- CI-Host/jobs/job.py
+++ CI-Host/jobs/job.py
@@ -100,60 +100,6 @@ class Job:
     # TODO: I think these static methods should be in the job queue.
 
     @staticmethod
-    def create_jobs_from_pipeline( uac, project, complete_callback=None ):
-        """
-            Creates a list of jobs from the project pipeline config file
-            and inserts them into the queue if uac permits ALL jobs
-        :param uac:
-        :param project:
-        :param complete_callback:
-        :return: True if successful full otherwise False
-        """
-
-        # authorize the activity by attempting to load the pipeline config.
-        # if None is returned, either the project does not exist or the user
-        # does not have access.
-        pipeline_config = commonProject.get_project_pipeline( uac, project, v2_config=True )    # TODO: remove v2_config once change is complete.
-
-        if pipeline_config is None:
-            complete_callback( False, "Unable to create job. Failed to load pipeline config." ) # TODO: find out if its an access or project issue
-            return False
-
-        if "jobs" not in pipeline_config:
-            complete_callback( False, "Unable to create jobs. Jobs not defined in pipeline config" )
-            return False
-
-        output_message = ""             # output message to be pushed into the complete callback.
-        jobs_to_queue = []
-        jobs = pipeline_config["jobs"]
-        job_names = list(jobs["jobs"])
-
-        for job in job_names:
-            # check stages has been defined and it contains at least one stage.
-            if "stages" not in jobs[job]:
-                output_message += f"Skipping job '{job}' stages not defined in pipeline\n"
-                continue    # skip job. stages not defined in pipeline.
-            elif len( jobs[job]["stages"] ) == 0:
-                output_message += f"Skipping job '{job}' contains no stages\n"
-                continue
-
-            stages = jobs[job]["stages"]
-            created_job, message = Job.create_job_of_tasks( uac, project, stages )
-
-            output_message += message
-
-            if created_job is None:
-                complete_callback( False, output_message )
-                return False
-
-            jobs_to_queue.append( created_job )
-            # TODO: Set the new job into the previous job, so it can be released once the previous job is released
-
-        # TODO: Queue jobs.
-
-        return True
-
-    @staticmethod
     def create_job_of_tasks( uac, project, stages, complete_callback=None ):    # TODO: NOTE: im sure about this complete callback!
         """
             Creates a job of tasks to be performed on the project
--- CI-Host/jobs/job_queue.py
+++ CI-Host/jobs/job_queue.py
@@ -1,10 +1,12 @@
 import DEBUG
+import commonProject
 import threading
 import queue
 import jobs.job as job
 import time
 
 _print = DEBUG.LOGS.print
+
 
 class JobQueue:
 
@@ -41,6 +43,61 @@ class JobQueue:
             return
 
         self.__queue.put( job_to_queue )
+
+    def create_jobs_from_pipeline( self, uac, project, complete_callback=None ):
+        """
+            Creates and queues jobs from the project pipeline config file
+            if the uac permits ALL jobs
+        :param uac:
+        :param project:
+        :param complete_callback:
+        :return: True if successful full otherwise False
+        """
+
+        # authorize the activity by attempting to load the pipeline config.
+        # if None is returned, either the project does not exist or the user
+        # does not have access.
+        pipeline_config = commonProject.get_project_pipeline( uac, project, v2_config=True )    # TODO: remove v2_config once change is complete.
+
+        if pipeline_config is None:
+            complete_callback( False, "Unable to create job. Failed to load pipeline config." ) # TODO: find out if its an access or project issue
+            return False
+
+        if "jobs" not in pipeline_config:
+            complete_callback( False, "Unable to create jobs. Jobs not defined in pipeline config" )
+            return False
+
+        output_message = ""             # output message to be pushed into the complete callback.
+        jobs_to_queue = []
+        jobs = pipeline_config["jobs"]
+        job_names = list(jobs["jobs"])
+
+        for job in job_names:
+            # check stages has been defined and it contains at least one stage.
+            if "stages" not in jobs[job]:
+                output_message += f"Skipping job '{job}' stages not defined in pipeline\n"
+                continue    # skip job. stages not defined in pipeline.
+            elif len( jobs[job]["stages"] ) == 0:
+                output_message += f"Skipping job '{job}' contains no stages\n"
+                continue
+
+            stages = jobs[job]["stages"]
+            created_job, message = job.Job.create_job_of_tasks( uac, project, stages )
+
+            output_message += message
+
+            if created_job is None:
+                complete_callback( False, output_message )
+                return False
+
+            jobs_to_queue.append( created_job )
+            # TODO: Set the new job into the previous job, so it can be released once the previous job is released
+
+        # queue jobs.
+        for j in jobs_to_queue:
+            self.queue_job( j )
+
+        return True
 
     def process_forever(self):
         """ Processes the job queue until exited """
