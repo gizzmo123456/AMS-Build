9562ed8f221a1ffd615c5aee311bb5dbbc40b0db
Ashley Sands <ashsands1@amstrike.com>
2021-06-19 20:43:02 +0100

    change debuiogn position

--- CI-Host/terminal.py
+++ CI-Host/terminal.py
@@ -55,7 +55,7 @@ class Terminal:
         while self.waitingForOutput:
             output += os.read( self.stdout.fileno(), 1024 ).decode()
 
-            _print( f"'{output[ -len(self.input_str): ]}' == '{self.input_str}'" )
+            _print( f"'{output[ -len(self.input_str): ]}' == '{self.input_str}' = {output[ -len(self.input_str): ] == self.input_str}" )
 
             if output[ -len(self.input_str): ] == self.input_str:
                 output = output[ len(input_cmd):-len(self.input_str)-1 ] # remove the inputed command and end input string from the output
