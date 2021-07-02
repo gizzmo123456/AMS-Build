47e94707d3ac140866a0eea9bf1906ad69cfaff9
Ashley Sands <ashsands1@amstrike.com>
2021-07-02 18:20:30 +0100

    added SHA to Hash

--- CI-Host/cipher.py
+++ CI-Host/cipher.py
@@ -5,6 +5,8 @@ from Crypto.Cipher import AES as crypto_aes
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import BLAKE2b
 
+import hashlib
+
 class Cipher:
 
     def __init__( self, secrets_path, cipher_name, key_length=16 ):
@@ -50,6 +52,12 @@ class Hash( Cipher ):
         self.hasher.update( data_str.encode() )
 
         return self.hasher.hexdigest()
+
+    @staticmethod
+    def sha1(string):
+        s = hashlib.sha1()
+        s.update(string.encode())
+        return s.hexdigest()
 
 # TODO: Update AES to support the base Cipher class
 class AES:
