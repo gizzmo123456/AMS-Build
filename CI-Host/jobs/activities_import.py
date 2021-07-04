1277e7569b7d3ed5cc3496bdcac785d3f31a3349
Ashley Sands <ashsands1@amstrike.com>
2021-07-04 20:52:42 +0100

    added empty class for prepare

--- CI-Host/jobs/activities_import.py
+++ CI-Host/jobs/activities_import.py
@@ -1,6 +1,7 @@
 # this file is used to import all activities, so they are added to the activities dictionary respectively in Job.
 
 # Tasks
+import jobs.tasks.prepare
 
 # Actions
 
---
+++ CI-Host/jobs/tasks/prepare.py
@@ -0,0 +1,16 @@
+import jobs.base_activity as base_activities
+
+
+class Prepare( base_activities.BaseTask ):
+
+    def init(self):
+        pass
+
+    def set_stage_data(self, data):
+        pass
+
+    def execute(self):
+        pass
+
+    def terminate(self):
+        pass
