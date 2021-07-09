612ca1ff32150b63eb92baa0120e326ad1043f74
Ashley Sands <ashsands1@amstrike.com>
2021-07-09 17:20:30 +0100

    Change docker common method to class and add stop and remove containers

--- CI-Host/commonTerminal.py
+++ CI-Host/commonTerminal.py
@@ -44,34 +44,89 @@ def ssh_agent_close( terminal, prefix_label="", **print_options ):
 
 
 # Docker
-def docker_image_exist_locally( terminal, image_name, prefix_label="", **print_options ):
+class Docker:
 
-    output = terminal_print( terminal, f"sudo docker image inspect {image_name}", prefix_label=prefix_label, **print_options)
+    def __init__(self, terminal, image_name, print_label="", **print_options):
 
-    # if the image does not exist, an emtpy array is return followed by an error message.
-    # otherwise the array contains the image info
+        self.terminal = terminal
+        self.image_name = image_name  # image-name:tag
+        self.print_label = print_label
+        self.print_options = print_options
 
-    # so basically we can just check if the first 2 characters of the output are not '[]'
+        self.container_name = None
 
-    return output[:2] != "[]"
+    def image_exist_locally( self ):
 
+        output = terminal_print( self.terminal, f"sudo docker image inspect {self.image_name}",
+                                 prefix_label=self.print_label, **self.print_options)
 
-def docker_pull_image( terminal, image_name, prefix_label="", **print_options ):
+        # if the image does not exist, an emtpy array is return followed by an error message.
+        # otherwise the array contains the image info
 
-    output = terminal_print( terminal, f"sudo docker pull {image_name}" )
+        # so basically we can just check if the first 2 characters of the output are not '[]'
 
-    # if the first word of the output is error we have failed to pull the docker image.
-    # Note: we only support pulling from public registries.
+        return output[:2] != "[]"
 
-    return output[:5].lower() != "error"
+    def pull_image( self ):
 
+        output = terminal_print( self.terminal, f"sudo docker pull {self.image_name}",
+                                 prefix_label=self.print_label, **self.print_options )
 
-def docker_run(terminal, image_name, args, prefix_label="", **print_options):
+        # if the first word of the output is error we have failed to pull the docker image.
+        # Note: we only support pulling from public registries.
 
-    output = terminal_print( terminal, f"sudo docker run {args} {image_name}")  # TODO: <<<
+        return output[:5].lower() != "error"
 
-def docker_stop(terminal, container_name, prefix_label="", **print_options):
-    pass
+    def run(self, container_name, args):
 
-def docker_kill(terminal, container_name, prefix_label="", **print_options):
-    pass
+        if self.container_name is not None:
+            _print( f"{self.print_label} Unable to run container, already running. ", **self.print_options )
+            return
+
+        self.container_name = container_name
+        output = terminal_print( self.terminal, f"sudo docker run --name {container_name} {args} {self.image_name}",
+                                 prefix_label=self.print_label, **self.print_options )  # TODO: <<<
+
+    def stop(self, timeout=10, kill=False):
+        """
+            sends a SIGTERM to the container. if the container does not exit within the timeout period
+            a SIGKILL is send to kill the process.
+            if kill is True. Just a SIGKILL is sent.
+        """
+
+        method = f"stop" if not kill else "kill"
+        timeout = f"-t {timeout}" if not kill else ""
+
+        if self.container_name is None:
+            _print( f"{self.print_label} Unable to {method} container. Container name not set.",
+                    prefix_label=self.print_label, **self.print_options )
+
+        output = terminal_print( self.terminal, f"sudo docker {method} {timeout} {self.container_name}",
+                                 prefix_label=self.print_label, **self.print_options )
+
+        # this should return the container name if stopped successfully
+
+        if output != self.container_name:
+            _print( f"{self.print_label} Failed to {method} container {self.container_name}", message_type=DEBUG.LOGS.MSG_TYPE_WARNING, **self.print_options )
+            return False
+
+        # now we can remove the container allowing the same name to be used again.
+        success = self.remove_container()
+
+        if success:
+            self.container_name = None
+
+        return success
+
+    def remove_container(self):
+
+        # remove the container
+        output = terminal_print(self.terminal, f"sudo docker container rm {self.container_name}",
+                                prefix_label=self.print_label, **self.print_options)
+
+        # this should return the container name if successful
+        if output != self.container_name:
+            _print(f"{self.print_label} Failed to remove container ({self.container_name})", **self.print_options)
+            return False
+
+        return True
