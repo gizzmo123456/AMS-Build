6ee7898617e213d1a2c9364f7f629862660ce5c7
Ashley Sands <as223343@falmouth.ac.uk>
2022-01-16 16:45:18 +0000

    Added basic python and pip checks to sh install script

--- ams-build-setup.sh
+++ ams-build-setup.sh
@@ -6,6 +6,24 @@
 
 ROOT_DIRECTORY=$PWD
 
+# check that python3 and pip3 are both installed
+pythonInstalled=$(python3 --version|grep -oP "Python 3")  # NOTE: we could do with checking for a mim 3.x vresion
+
+if [ "$pythonInstalled" = "Python 3" ]; then
+  echo "Python 3 is installed!"
+else
+  echo "Python 3 is not installed. Please install python3 first then run this script again (Recommended version 3.9)"
+  exit 1
+fi;
+
+pipInstalled=$(pip3 -V|grep -oP "python 3")
+
+if [ "$pipInstalled" = "python 3" ]; then
+  echo "Pip 3 is installed!"
+else
+  echo "Pip 3 is not installed. Please install pip3 (sudo apt install python3-pip) before continuing"
+  exit 1
+
 echo "Installing Python 3 Requirements..."
 pip3 install -r ./requirements.txt
 
